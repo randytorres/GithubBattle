da26d9d1a8bb658de06dd9363870f984776eb06e
Randy Torres <torresr14@gmail.com>
2016-03-03 00:11:26 -0800

    created ConfirmBattle.js & added life cycle events

--- app/containers/PromptContainer.js
+++ app/containers/PromptContainer.js
@@ -24,14 +24,14 @@ var PromptContainer = React.createClass({
 		});
 
 		if (this.props.routeParams.playerOne) {
-		  	this.context.router.push({
-					pathname: '/battle',
-					query: {
-						playerOne: this.props.routeParams.playerOne,
-						playerTwo: this.state.username
-					}
+		  this.context.router.push({
+				pathname: '/battle',
+				query: {
+					playerOne: this.props.routeParams.playerOne,
+					playerTwo: this.state.username
+				}
 
-				})
+			})
 		} else {
 			this.context.router.push('/playerTwo/' + this.state.username)
 		}
